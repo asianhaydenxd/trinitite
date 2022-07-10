94ce9767f4040867ef551c0ecd177eecdf1c021a
Hayden <asianhaydenxd@gmail.com>
2022-07-10 02:21:04 -0400

    Fetch messages from SMS

--- sms.py
+++ sms.py
@@ -8,11 +8,18 @@ class Trinitite:
         self.ip = ip
         self.session = AirmoreSession(self.ip)
         self.service = MessagingService(self.session)
+        self.fetched = []
+        self.fetch_msgs()
     
     def fetch_msgs(self):
         all_history = self.service.fetch_message_history()
-        history = self.service.fetch_chat_history([message for message in all_history if message.phone == self.phone][0])
-        return history
+        history = self.service.fetch_chat_history([message for message in all_history if message.phone in (self.phone, "+1" + self.phone)][0])
+        new_messages = []
+        for message in reversed(history):
+            if message not in self.fetched:
+                new_messages.append(message)
+        self.fetched = new_messages + self.fetched
+        return new_messages
     
     def send_msg(self, msg: str):
         self.service.send_message(self.phone, msg)
